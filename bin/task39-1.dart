17888bb3757ce0266eb1ef1fc67d1af92e84e324
abdulakh <magomedovabdullah20042007@mail.ru>
2022-01-24 19:53:06 +0300

    txt

--- bin/task39-1.dart
+++ bin/task39-1.dart
@@ -1,13 +1,35 @@
+void main() {
+  Hero chort = Hero()
+    ..name='chort'
+    ..health=30
+    ..energy=30
+    ..damage=90;
+
+  print(chort);
+}
+
 class Hero
 {
-  String name;
-  int health;
-  int energy;
-  int damage;
+  String name='';
+  int health=50;
+  int energy=50;
+  int damage=50;
 
-  Hero(this.name,this.health,this.energy,this.damage);
-  void attack()
+  Hero();
+
+  Hero.warrior(this.name)
   {
+    health=60;
+    energy=20;
+    damage=70;
+  }
 
+  Hero.rogue(this.name,[this.health=50,this.energy=80,this.damage=20]);
+
+  Hero.healer({required this.name,this.health=90,this.energy=30,this.damage=30});
+
+  @override
+  String toString() {
+    return "Hero $name \nhealth = $health \nenergy = $energy \ndamage = $damage \n";
   }
 }
