7e5371194b25f5c3843ca87c4b9105fed0026832
Flutter <magomedovabdullah20042007@mail.ru>
2022-03-23 19:57:29 +0300

    task65

---
+++ bin/les65_1.dart
@@ -0,0 +1,22 @@
+void main()
+{
+  Poster nature=Poster(13, 16.6, 591);
+  print(nature);
+
+  Poster <int, String> vintage = Poster(62, 27, '184');
+  print(vintage);
+  print(vintage.length+nature.width);
+}
+
+class Poster <L extends num, P>{
+  L width;
+  L length;
+
+  Poster(this.length,this.width, this.price);
+
+  P price;
+  @override
+  String toString(){
+    return '$width $length $price';
+  }
+}
